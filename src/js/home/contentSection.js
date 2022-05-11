d5b474f07574bb76cc505799eb53f6b27f489e17
Harold <cvuyante24@gmail.com>
2022-05-11 14:26:02 +0800

    Second day / Second Week

--- src/js/home/contentSection.js
+++ src/js/home/contentSection.js
@@ -3,14 +3,54 @@ import gundamImage from '../../images/homeGundam.jpg';
 
 export const contentComponents = () => {
 	const div = document.createElement('div');
+	const div1 = document.createElement('div');
+	const div2 = document.createElement('div');
+	const div3 = document.createElement('div');
+	const h1 = document.createElement('h1');
+	const para = document.createElement('p');
+	const button = document.createElement('button');
+
 	const myImage = new Image();
 
 	myImage.src = gundamImage;
+	h1.textContent = 'Gundam Cafe';
+	para.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Donec facilisis massa eget mauris dictum ultrices in id 
+		justo. Suspendisse vel ornare quam. Vestibulum ante ipsum 
+		primis in faucibus orci luctus et ultrices posuere cubilia
+		curae; Donec a tempor eros. Ut viverra laoreet leo at 
+		aliquet. Aenean a varius nisl. Interdum et malesuada fames
+		ac ante ipsum primis in faucibus. Maecenas ut risus vel
+		ante dictum euismod. Sed malesuada tortor libero, ut 
+		hendrerit augue rutrum aliquet.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Donec facilisis massa eget mauris dictum ultrices in id 
+		justo. Suspendisse vel ornare quam. Vestibulum ante ipsum 
+		primis in faucibus orci luctus et ultrices posuere cubilia
+		curae; Donec a tempor eros. Ut viverra laoreet leo at 
+		aliquet. Aenean a varius nisl. Interdum et malesuada fames
+		ac ante ipsum primis in faucibus. Maecenas ut risus vel
+		ante dictum euismod. Sed malesuada tortor libero, ut 
+		hendrerit augue rutrum aliquetLorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Donec facilisis massa eget mauris dictum ultrices in id 
+		justo. Suspendisse vel ornare quam. Vestibulum ante ipsum 
+		primis in faucibus orci luctus et ultrices posuere cubilia
+		curae; Donec a tempor eros.`;
+	button.textContent = 'Menu';
 
 	div.classList.add('contentContainer');
-	myImage.classList.add('contentContainer__image');
+	div1.classList.add('contentContainer__first');
+	div2.classList.add('contentContainer__second');
+	div3.classList.add('contentContainer__third');
+	myImage.classList.add('contentContainer__first--image');
+	button.classList.add('contentContainer__third--button');
 
-	div.appendChild(myImage);
+	div1.appendChild(myImage);
+	div2.appendChild(h1);
+	div2.appendChild(para);
+	div3.appendChild(button);
+	div.appendChild(div1);
+	div.appendChild(div2);
+	div.appendChild(div3);
 
 	return div;
 };
