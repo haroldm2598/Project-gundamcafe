0f092bec9d7093779bae773c5151561c8270b065
Harold <cvuyante24@gmail.com>
2022-05-16 12:03:34 +0800

    First day / Third Week

--- src/js/menu/contentMenu.js
+++ src/js/menu/contentMenu.js
@@ -3,10 +3,11 @@ import json from './data.json';
 
 export const contentComponents = () => {
 	const div = document.createElement('div');
+	const data = json.details;
 
 	div.classList.add('menuContainer');
 
-	for (let i = 0; i < 2; i++) {
+	for (let i = 0; i < data.length; i++) {
 		const div1 = document.createElement('div');
 		const div2 = document.createElement('div');
 		const div3 = document.createElement('div');
@@ -18,17 +19,14 @@ export const contentComponents = () => {
 		div2.classList.add('menuContainer__item--img');
 		div3.classList.add('menuContainer__item--content');
 
-		h3.textContent = 'LOREM IPSUM';
-		para.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-		Donec facilisis massa eget mauris dictum ultrices in id 
-		justo. Suspendisse vel ornare quam.`;
+		h3.textContent = data[i].title;
+		para.textContent = data[i].about;
 
 		div1.appendChild(div2);
 		div1.appendChild(div3);
 		div2.appendChild(image);
 		div3.appendChild(h3);
 		div3.appendChild(para);
-
 		div.appendChild(div1);
 	}
 
