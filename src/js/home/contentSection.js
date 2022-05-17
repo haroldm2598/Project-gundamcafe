b7753bc63e66c27be2b37899f3432e7465085025
Harold <cvuyante24@gmail.com>
2022-05-17 14:38:55 +0800

    Second day / Third Week

--- src/js/home/contentSection.js
+++ src/js/home/contentSection.js
@@ -28,13 +28,7 @@ export const contentComponents = () => {
 		primis in faucibus orci luctus et ultrices posuere cubilia
 		curae; Donec a tempor eros. Ut viverra laoreet leo at 
 		aliquet. Aenean a varius nisl. Interdum et malesuada fames
-		ac ante ipsum primis in faucibus. Maecenas ut risus vel
-		ante dictum euismod. Sed malesuada tortor libero, ut 
-		hendrerit augue rutrum aliquetLorem ipsum dolor sit amet, consectetur adipiscing elit.
-		Donec facilisis massa eget mauris dictum ultrices in id 
-		justo. Suspendisse vel ornare quam. Vestibulum ante ipsum 
-		primis in faucibus orci luctus et ultrices posuere cubilia
-		curae; Donec a tempor eros.`;
+		ac ante ipsum primis in faucibus.`;
 	button.textContent = 'Menu';
 
 	div.classList.add('contentContainer');
