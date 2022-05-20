111578a656415a52d14ba404dfb65645ea80f6ed
Harold <cvuyante24@gmail.com>
2022-05-20 14:26:31 +0800

    Friday day / Third Week

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -1,9 +1,11 @@
 import { navbarComponent } from '../reusableComponents/navbarSection';
 import { footerComponent } from '../reusableComponents/footerSection';
 import { contentComponents } from '../menu/contentMenu';
+import { funcEvent } from '../eventListener/index';
 
 export const menuComponents = () => {
 	document.body.appendChild(navbarComponent());
 	document.body.appendChild(contentComponents());
 	document.body.appendChild(footerComponent());
+	funcEvent();
 };
