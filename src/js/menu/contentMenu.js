ee2ca6901056fde511b57c2118be87649338d5be
Harold <cvuyante24@gmail.com>
2022-05-25 14:27:54 +0800

    Third Day / Fourth Week

--- src/js/menu/contentMenu.js
+++ src/js/menu/contentMenu.js
@@ -1,9 +1,24 @@
 import '../../styles/main.scss';
 import json from './data.json';
 
+// https://api.unsplash.com/search/photos?query=minimal
+
 export const contentComponents = () => {
+	const clientId = 'rYIji7X0Fh9YOV1m8NBErYTgqLoNB9bnE18d0HP_VlU';
+	const url =
+		'https://api.unsplash.com/search/photos?query=restaurant&client_id=' +
+		clientId;
+
 	const div = document.createElement('div');
 	const data = json.details;
+
+	// (async () => {
+	// 	let getImg = await getImage();
+	// 	const testingImg = new Image();
+
+	// 	testingImg.src = getImg;
+	// 	div.appendChild(testingImg);
+	// })();
 
 	div.classList.add('menuContainer');
 
@@ -28,6 +43,16 @@ export const contentComponents = () => {
 		div3.appendChild(h3);
 		div3.appendChild(para);
 		div.appendChild(div1);
+	}
+
+	async function getImage() {
+		let randomNumber = Math.floor(Math.random() * 6);
+		return fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				let allImages = data.results[randomNumber];
+				return allImages.urls.regular;
+			});
 	}
 
 	return div;
