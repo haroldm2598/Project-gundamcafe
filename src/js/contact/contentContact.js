bf1ffd3cbf5f0c8df2e27ed41fa0c9d335136944
Harold <cvuyante24@gmail.com>
2022-05-30 15:06:08 +0800

    first day / last week

--- src/js/contact/contentContact.js
+++ src/js/contact/contentContact.js
@@ -1,28 +1,34 @@
 import '../../styles/main.scss';
-import json from './data.json';
+import { contactArr } from './data';
+import streetMap from '../../images/streetMap.jpg';
 
 export const contentComponents = () => {
 	const div = document.createElement('div');
 	const mapSection = document.createElement('div');
 	const contactSection = document.createElement('div');
 	const ul = document.createElement('ul');
+	const myImage = new Image();
+
+	myImage.src = streetMap;
 
 	div.classList.add('contactContainer');
 	mapSection.classList.add('contactContainer__mapSection');
 	contactSection.classList.add('contactContainer__contactSection');
+	ul.classList.add('contactContainer__contactSection--list');
 
-	for (let i = 0; i < json.length; i++) {
+	for (let i = 0; i < contactArr.length; i++) {
 		const li = document.createElement('li');
 
-		li.textContent = json[i].address;
-		li.textContent = json[i].email;
-		li.textContent = json[i].phoneNumber;
+		li.textContent = `address ${contactArr[i]}`;
+		li.textContent = `email ${contactArr[i]}`;
+		li.textContent = `phone number ${contactArr[i]}`;
 
 		ul.appendChild(li);
 	}
 
 	div.appendChild(mapSection);
 	div.appendChild(contactSection);
+	mapSection.appendChild(myImage);
 	contactSection.appendChild(ul);
 
 	return div;
---
+++ src/js/contact/data.js
@@ -0,0 +1,5 @@
+export const contactArr = [
+	'239, st joseph square urdonisan',
+	'caffeein_whey@yahho.com',
+	'+69 8745 4653 154'
+];
